fd7cb110c06e2bc4b7985b754c4cb2677704ddd7
A_Bot <a_bot@github.com>
2018-06-19 22:10:10 -0400

    Refactoring code for Contact addition tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -11,12 +11,14 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private ContactHelper contactHelper;
 
     public void init() {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
+        contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login("admin", "secret");
@@ -30,11 +32,18 @@ public class ApplicationManager {
         return groupHelper;
     }
 
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
+
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
     }
 
     public void gotoGroupPage() {
         navigationHelper.gotoGroupPage();
+    }
+
+    public void gotoContactPage() { navigationHelper.gotoAddContactPage();
     }
 }
