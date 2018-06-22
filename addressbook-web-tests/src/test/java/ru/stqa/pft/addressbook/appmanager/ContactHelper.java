ed1565501b6bbc69dc304ac28c84030ed8a9f602
A_Bot <a_bot@github.com>
2018-06-21 22:06:36 -0400

    ContactModificationTests / ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -25,4 +25,24 @@ public class ContactHelper extends HelperBase {
         type(By.name("home"), contactData.getPhone());
         type(By.name("email"), contactData.getEmail());
     }
+
+    public void initContactEdit() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void updateContactInfo() {
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+    }
+
+    public void deleteContact() {
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+    }
+
+    public void selectContact() {
+            wd.findElement(By.name("selected[]")).click();
+    }
+
+    public void confirmDeletingContact() {
+        wd.switchTo().alert().accept();
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,15 +3,14 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-
-public class ContactCreationTests extends TestBase {
+public class ContactModificationTests extends TestBase {
 
     @Test
-    public void testContactCreation() {
-        app.gotoContactPage();
+
+    public void testContactModification() {
+        app.getContactHelper().initContactEdit();
         app.getContactHelper().fillContactForm(new ContactData("John", "Travolta", "18887779090", "johntravolta777@gmail.com"));
-        app.getContactHelper().enterNewContact();
+        app.getContactHelper().updateContactInfo();
         app.getContactHelper().returnToHomePage();
     }
-
 }
