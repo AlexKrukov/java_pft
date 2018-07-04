5467d79756f53d877d0aa32eb4cdf329bbd76f86
A_Bot <a_bot@github.com>
2018-07-03 22:00:35 -0400

    Lambda, Comparator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,19 +3,20 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -24,7 +25,7 @@ public class GroupData {
 
 
 
-    public String getId() { return id; }
+    public int getId() { return id; }
 
     public String getName() {
         return name;
@@ -36,6 +37,10 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
@@ -51,15 +56,13 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
-                Objects.equals(name, groupData.name) &&
-                Objects.equals(header, groupData.header) &&
-                Objects.equals(footer, groupData.footer);
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, name, header, footer);
+        return Objects.hash(id, name);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import com.sun.tools.javac.util.List;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 
 
 public class GroupCreationTests extends TestBase {
@@ -14,9 +15,14 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.gotoGroupPage();
         ArrayList<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test2", null, null);
+        app.getGroupHelper().createGroup(group);
         ArrayList<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
