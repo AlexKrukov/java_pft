8c7d2d005a4dca8388c8cdec7c6f500cc657cc88
A_Bot <a_bot@github.com>
2018-07-16 22:43:52 -0400

    5.8 Hashing

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -26,7 +26,7 @@ public class ContactDelitionTests extends TestBase {
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
         Contacts after = app.contact().all();
-        assertEquals(after.size(), before.size() - 1);
+        assertThat(app.contact().count(), equalTo(before.size() -1));
         assertThat(after, equalTo(before.without(deletedContact)));
     }
 
